65897ca3a3bb67eec968ddd5f1f18a18cb3d9bb3
ily6ix <goitseonetrade@gmail.com>
2025-11-06 10:02:18 +0200

    Fix deployment

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.get('/', (req, res) => {
 
 // Test Supabase connection
 testSupabaseConnection();
-module.export = app;
+module.exports = app;
 
 //const PORT = 3000;
 //app.listen(PORT, () => {
